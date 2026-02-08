daae01da4ba1e634e1b6fc06ed5a2c6e94aaee94
RenatoHioji <okamoto.hioji@gmail.com>
2026-02-07 21:20:35 -0300

    feat: adding bussiness logic to event

--- src/main/java/com/example/api/event/domain/Event.java
+++ src/main/java/com/example/api/event/domain/Event.java
@@ -2,6 +2,7 @@ package com.example.api.event.domain;
 
 import com.example.api.event.domain.enums.EventStatus;
 import com.example.api.shared.BaseEntity;
+import com.example.api.shared.BussinessException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,4 +26,45 @@ public class Event extends BaseEntity {
     private EventStatus eventStatus;
     private LocalDateTime startedAt;
     private LocalDateTime canceledAt;
+
+    private void close(){
+        this.eventStatus = EventStatus.CLOSED;
+    }
+
+    private void open(){
+        if(this.eventStatus.equals(EventStatus.CANCELED)){
+            this.eventStatus = EventStatus.OPEN;
+        }
+        throw new BussinessException("Não é possível reabrir um evento que não está cancelado.");
+    }
+
+    private void start(){
+        if(this.eventStatus.equals(EventStatus.FULL) || eventStatus.equals(EventStatus.OPEN)){
+            this.eventStatus = EventStatus.ON_GOING;
+        }
+        throw new BussinessException("Não é possível iniciar um evento que não esteja cheio ou não esteja aberto");
+    }
+
+    private void cancel(){
+        if(!this.eventStatus.equals(EventStatus.COMPLETED)){
+            this.eventStatus = EventStatus.CANCELED;
+        }
+        throw new BussinessException("Não é possível cancelar um evento completo");
+    }
+
+    private void complete(){
+        if(this.eventStatus.equals(EventStatus.ON_GOING)){
+            this.eventStatus = EventStatus.COMPLETED;
+        }
+        throw new BussinessException("Não é possível completar um evento que não esteja em andamento");
+
+    }
+
+    private void full(){
+        if(this.eventStatus.equals(EventStatus.OPEN)){
+            this.eventStatus = EventStatus.FULL;
+        }
+
+        throw new BussinessException("Não é possível indicar como cheio um evento que não está como aberto");
+    }
 }
